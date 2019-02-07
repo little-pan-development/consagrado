3e1160c5a4a1c5ca890373b84fc8029265a79832
Bruno Bandeira <brunoban.dev@gmail.com>
2019-02-07 21:24:12 -0200

    clear utils.go

--- app/utils.go
+++ app/utils.go
@@ -5,45 +5,6 @@ import (
 	"github.com/palmirinha/app/models"
 )
 
-// func (app *App) getCartContentsAsEmbed(channelID string, s *discordgo.Session) *discordgo.MessageEmbed {
-// 	var cart Cart
-// 	row := app.Connection.QueryRow("SELECT id, description FROM cart WHERE status = 1 and channel_id = ?", channelID)
-// 	err := row.Scan(&cart.ID, &cart.Description)
-
-// 	rows, err := app.Connection.Query("SELECT description, discord_user_id FROM item WHERE cart_id = ?", cart.ID)
-
-// 	embed := &discordgo.MessageEmbed{}
-
-// 	embed.Title = "Pedidos até o momento:"
-// 	embed.Description = "**--** :hamburger: **--**"
-// 	embed.Color = 0xff0000
-
-// 	embed.Author = &discordgo.MessageEmbedAuthor{}
-// 	embed.Author.Name = "Palmirinha!"
-// 	embed.Author.URL = "https://www.facebook.com/vovopalmirinha/"
-// 	embed.Author.IconURL = "https://i.imgur.com/QTDVdLK.jpg"
-
-// 	embed.Fields = []*discordgo.MessageEmbedField{}
-
-// 	for rows.Next() {
-// 		var item Item
-// 		err = rows.Scan(&item.Description, &item.DiscordUserID)
-// 		if err != nil {
-// 			fmt.Println(err)
-// 		}
-
-// 		var user, _ = s.User(item.DiscordUserID)
-
-// 		embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-// 			Name:   "\n\n**" + user.Username + "**",
-// 			Value:  item.Description,
-// 			Inline: false,
-// 		})
-// 	}
-
-// 	return embed
-// }
-
 // EmbedListItems ...
 func EmbedListItems(list *models.List, bc *BotCommand) *discordgo.MessageEmbed {
 
