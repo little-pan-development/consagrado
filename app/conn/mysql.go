708074d031a815766339c872e37caaa5758f320b
Bruno Bandeira <brunoban.dev@gmail.com>
2019-02-12 22:11:00 -0200

    use env variables to mysql connect

--- app/conn/mysql.go
+++ app/conn/mysql.go
@@ -3,13 +3,23 @@ package conn
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 // Mysql ...
 func Mysql() *sql.DB {
-	conn, err := sql.Open("mysql", "palmirinha:palmirinha@tcp(palmirinha-db:3306)/palmirinha?charset=utf8mb4&collation=utf8mb4_unicode_ci")
+
+	database := os.Getenv("MYSQL_DATABASE")
+	username := os.Getenv("MYSQL_USERNAME")
+	password := os.Getenv("MYSQL_PASSWORD")
+	port := os.Getenv("MYSQL_PORT")
+	host := os.Getenv("MYSQL_HOST")
+
+	uri := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&collation=utf8mb4_unicode_ci", username, password, host, port, database)
+
+	conn, err := sql.Open("mysql", uri)
 	if err != nil {
 		fmt.Println("Conn Mysql: ", err)
 	}
